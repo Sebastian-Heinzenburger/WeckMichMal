e07ebdcd1bf280703167cc48b7263a56269a827b
Finn Hennemann <hennemann.finn.a23@student.dhbw-karlsruhe.de>
2025-05-07 10:10:06 +0200

    add docs for BatchProcessing.kt

--- app/src/main/java/de/heinzenburger/g2_weckmichmal/api/rapla/BatchProcessing.kt
+++ app/src/main/java/de/heinzenburger/g2_weckmichmal/api/rapla/BatchProcessing.kt
@@ -1,16 +1,40 @@
 package de.heinzenburger.g2_weckmichmal.api.rapla
 
+/**
+ * Represents an element in a batch, paired with a unique ID.
+ *
+ * @param T The type of the contained value.
+ * @property id The unique ID of this element within the batch.
+ * @property value The value associated with this ID.
+ */
 data class BatchTuple<T>(
     val id: Int,
     val value: T
 ) {
-    fun <I>map(value: I): BatchTuple<I>{
+    /**
+     * Creates a new [BatchTuple] with the same [id], but a different [value].
+     *
+     * @param I The type of the new value.
+     * @param value The new value to associate with the existing ID.
+     * @return A new [BatchTuple] instance with the updated value.
+     */
+    fun <I> map(value: I): BatchTuple<I> {
         return BatchTuple(id, value)
     }
 }
 
+/**
+ * A type alias for a collection of [BatchTuple] elements.
+ */
 typealias Batch<T> = Iterable<BatchTuple<T>>
 
+/**
+ * Performs an inner join on two batches based on matching IDs.
+ *
+ * @param originBatch The original batch containing elements of type [I].
+ * @param outputBatch The batch to join with, containing elements of type [O].
+ * @return A list of pairs where the IDs matched in both batches, containing values from each batch.
+ */
 fun <I, O> innerJoinBranches(
     originBatch: Batch<I>,
     outputBatch: Batch<O>
@@ -28,6 +52,13 @@ fun <I, O> innerJoinBranches(
     }
 }
 
+/**
+ * Performs an inner join on two batches, preserving the ID and structure in the result.
+ *
+ * @param originBatch The original batch containing elements of type [I].
+ * @param outputBatch The batch to join with, containing elements of type [O].
+ * @return A new batch containing [BatchTuple] elements with paired values from both batches.
+ */
 fun <I, O> innerJoinBranchesPreserving(
     originBatch: Batch<I>,
     outputBatch: Batch<O>
@@ -45,7 +76,13 @@ fun <I, O> innerJoinBranchesPreserving(
     }
 }
 
-fun <T>batchFrom(list: Iterable<T>) : Batch<T> {
+/**
+ * Creates a [Batch] from a given list by assigning each element a sequential ID.
+ *
+ * @param list The input list.
+ * @return A batch of [BatchTuple] elements with indexed IDs.
+ */
+fun <T> batchFrom(list: Iterable<T>): Batch<T> {
     return list.mapIndexed { index, value ->
         BatchTuple(id = index, value = value)
     }
